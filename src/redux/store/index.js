88b9d9b6b4c4b3484fae66a1b479fcd06827416d
Axel Tinoco <axeltm8@gmail.com>
2023-12-10 17:56:54 -0600

    Fix error in persist storage

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Tuple, configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import authReducer from '../features/authSlice';
 import initialReducer from '../features/initialSlice';
 import persistReducer from "redux-persist/es/persistReducer";
+import thunk from "redux-thunk";
+
 
 const reducers = combineReducers({
   auth: authReducer,
@@ -13,12 +15,14 @@ const reducers = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
+  timeout: 0
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export default store;
